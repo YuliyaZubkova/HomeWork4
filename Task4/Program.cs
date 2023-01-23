4ad2183d220d03dbea98185a33591ac9b02fe88f
Yuliya <i@yzubkova.ru>
2023-01-23 15:04:03 +0300

    Массив из 8 элементов заполненных в случайном порядке

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа, котоая выводит массив из 8 элементов заполненых 0 и 1 в случайном порядке
+
+int[] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write($"{array[i]}, ");
+}
