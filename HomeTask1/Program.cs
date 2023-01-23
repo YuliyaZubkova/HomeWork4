cd8a571a474ee3e2c935329d1fb08133a04bb5b8
Yuliya <i@yzubkova.ru>
2023-01-23 22:23:05 +0300

    Возводим первое число в степень второго числа число А в степень В)

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < numberTwo; i++)
+{
+    result = result*numberOne;
+}
+Console.WriteLine($"{numberOne}, {numberTwo} -> {result}");
+
