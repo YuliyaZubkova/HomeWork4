657233ddaa917305143dd6ca277f4c72729a1373
Yuliya <i@yzubkova.ru>
2023-01-23 14:30:04 +0300

    Программа выдает количество цифр в числе

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Программа которая принимает число и выдает количество цифр в числе
+
+/* Простой ввод числа с консоли
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.Readline());
+*/
+
+//Ввод числа с консоли с проверкой 
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result; 
+}
+
+int number = GetNumber("Введите число");
+
+
+int count = 0;
+
+while (number > 0)
+{
+    number = number / 10;
+    count++;
+}
+
+Console.WriteLine($"Количество цифр в числе: {count}");
