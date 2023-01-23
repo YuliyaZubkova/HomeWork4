a7c9193d1aaac738c199c9544316492bcc3abb01
Yuliya <i@yzubkova.ru>
2023-01-23 11:08:50 +0300

    Найти сумму числа от 1 до n

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+// получение числа с консоли
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result; 
+}
+
+int GetSumm(int number)
+{
+    int summ = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
+
+int number = GetNumber("Введите число");
+int summ = GetSumm(number);
+
+Console.WriteLine($"Сумма чисел от 1 до {number} = {summ}");
