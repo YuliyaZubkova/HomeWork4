6f80ffbe7b0d7be88a78bda0c6c06ccc868d7b6b
Yuliya <i@yzubkova.ru>
2023-01-23 22:45:09 +0300

    Программа выдает сумму цифр в числе

---
+++ HomeTask2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе. (можно написать по аналогии с задачей на факториал из лекции - Example)
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+string number1 = Console.ReadLine();
+int number = Convert.ToInt32(number1);
+int count = number.ToString().Length;
+
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int a, int b)
+{
+int result = 0;
+
+    for (int i = 0; i < b; i++)
+    {
+        result = result + a%10;
+        a = a/10;
+    }
+
+Console.Write($"{number} -> ");
+return result;
+
+}
+
