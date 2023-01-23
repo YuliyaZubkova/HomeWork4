567b386feb93ed845a84996e42258e2d7a2b735f
Yuliya <i@yzubkova.ru>
2023-01-23 14:50:43 +0300

    Программа выдает произведение чисел от 1 до N

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа которая принимает число N и выдает произведение от 1 до N
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result; 
+}
+
+int num = GetNumber("Введите число");
+int result = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    result = result * i;
+}
+Console.WriteLine($"Произведение от 1 до {num} = {result}");
