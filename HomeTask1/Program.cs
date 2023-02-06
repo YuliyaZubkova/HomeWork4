fe9c610831f4b3b54107af758ba2f148d848082f
Yuliya <i@yzubkova.ru>
2023-02-06 11:58:15 +0300

    Возвести число А в степень числа В

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,20 +1,52 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A в натуральную степень B.
 
 //3, 5 -> 243 (3⁵)
 
 //2, 4 -> 16
 
-Console.WriteLine("Введите первое число: ");
-int numberOne = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int numberTwo = Convert.ToInt32(Console.ReadLine());
 
-int result = 1;
-
-for (int i = 0; i < numberTwo; i++)
+int GetNumber(string message)
 {
-    result = result*numberOne;
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
 }
-Console.WriteLine($"{numberOne}, {numberTwo} -> {result}");
+
+int GetDegree(int numberOne, int numberTwo)
+{
+    int result = 1;
+
+    for (int i = 0; i < numberTwo; i++)
+    {
+        result = result * numberOne;
+    }
+
+    return result;
+
+}
+
+int numberOne = GetNumber("Введите первое число: ");
+int numberTwo = GetNumber("Введите второе число: ");
+
+Console.WriteLine($"Число {numberOne} в степени {numberTwo} равно {GetDegree(numberOne, numberTwo)}.");
+
+
+
+
+
 
