1ecb5e997409f02c370c9c309045836b60f0f63e
Yuliya <i@yzubkova.ru>
2023-02-06 12:19:24 +0300

    Программа выводит на экран массив из 8 элементов

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -1,28 +1,6 @@
 ﻿/* Напишите программу, которая задаёт массив из 8 элементов 
 и выводит их на экран. (Задача из какой то лекции  кажется из Examples2)
-
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
-6, 1, 33 -> [6, 1, 33]
 */
-
-/* int[] arr = {1, 2, 5, 7, 19, 6, 1, 33};
-
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-}
-
-PrintArray(arr);
-Console.Write(" -> [");
-PrintArray(arr);
-Console.Write("]"); */
-
 
 int[,] numbers = { { 1, 2, 5, 7, 19, 6, 1, 33 }};
 foreach (int i in numbers)
